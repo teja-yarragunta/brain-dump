c5f356d8e03b593b6888af085af0920fb7ac1561
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-11-24 15:23:12 +0530

    brain dump

--- Intro to SQL/2. Creating and Joining Tables/notes.md
+++ Intro to SQL/2. Creating and Joining Tables/notes.md
@@ -5,3 +5,17 @@ types of relationships:
     one to one
     one to many
     many to many
+
+```
+JOINS
+```
+we can retrieve data from multiple tables using the JOIN clauses
+we will select data from each table, joining tables on columns they have in common
+our tables need to have common columns which we can include in our join clause
+
+4 types on JOIN in SQL
+    1) INNER JOIN
+    2) LEFT JOIN
+    3) RIGHT JOIN
+    4) FULL JOIN
+each will return a different set of results, depending on what matches
--- Intro to SQL/2. Creating and Joining Tables/query.sql
+++ Intro to SQL/2. Creating and Joining Tables/query.sql
@@ -1,1 +1,42 @@
-SELECT id, dealership_id, brand, model FROM cars;
+-- SELECT * FROM sold_cars;
+
+-- JOINS
+
+
+-- LEFT JOIN
+/*
+	Select the brand, model, price, sold, sold_price columns
+		from sold_cars
+	Left join with cars
+		matching sold_cars.cars_id to cars.id
+*/
+-- SELECT brand, model, price, sold, sold_price FROM sold_cars
+--     LEFT JOIN cars ON sold_cars.cars_id = cars.id;
+
+-- SELECT brand, model, price, sold, sold_price FROM sold_cars sc
+--     LEFT JOIN cars c ON sc.cars_id = c.id;
+
+
+/*
+	Select name, role, city, state
+		From the staff table
+	Left join with the dealerships table where the dealership_id in staff
+			matches with the id in dealerships
+*/
+-- SELECT name, role, city, state FROM staff 
+--     LEFT JOIN dealerships d ON dealership_id=d.id
+
+
+-- RIGHT JOIN
+/*
+	Select the brand, model, price, sold, sold_price columns
+		from sold_cars
+	Right join with cars
+		matching sold_cars.cars_id to cars.id
+*/
+-- SELECT brand, model, price, sold, sold_price 
+--   FROM sold_cars SC
+--   RIGHT JOIN cars C ON SC.cars_id = C.id;
+
+
+
