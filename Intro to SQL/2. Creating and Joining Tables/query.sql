e904dc7438089d19a6ad05d0eedb335aa21dd6fa
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-11-25 22:30:39 +0530

    2. Creating and Joining Tables

--- Intro to SQL/2. Creating and Joining Tables/query.sql
+++ Intro to SQL/2. Creating and Joining Tables/query.sql
@@ -1,4 +1,4 @@
--- SELECT * FROM sold_cars;
+-- SELECT * FROM dealerships;
 
 -- JOINS
 
@@ -40,3 +40,186 @@
 
 
 
+-- FULL JOIN & INNER
+/*
+	Select name, role from staff and city, state from dealerships
+	Join the staff table to dealerships using full join
+		match the staff.dealership_id to dealerships.id
+*/
+
+-- SELECT name, role, city, state FROM staff
+-- 	FULL JOIN dealerships ON dealership_id = dealerships.id;
+
+-- SELECT name, role, city, state FROM staff
+-- 	INNER JOIN dealerships ON dealership_id = dealerships.id;
+
+
+/*
+	Select name, role, sold_price from staff
+	Inner join with sold_cars
+		matching seller with staff.id	
+*/
+-- SELECT name, role, sold_price FROM staff
+-- 	INNER JOIN sold_cars ON staff.id = seller;
+
+-- SELECT name, role, sold_price FROM staff
+-- 	FULL JOIN sold_cars ON staff.id = seller;
+
+
+
+
+
+
+
+/*
+	Select the city and average car price
+	Round that car price to a whole number
+	
+	Only show dealerships which have cars
+	
+	Group by dealership city and state
+*/
+-- SELECT city, state, ROUND(AVG(price), 2) AS avg_price
+-- 	FROM cars
+-- 	LEFT JOIN dealerships D ON dealership_id = D.id
+-- GROUP BY city, state;
+
+
+
+/*
+	Select the name and role, alongside a total_sales:
+		this is the sum of sales by a member of staff
+	
+	Use staff as your left table and sold_cars as your right table
+	
+	Include a where clause to select only staff with the role 'Salesperson'
+	
+	Group by staff name and role
+	Order by the total_sales from high to low
+*/
+-- SELECT name, role, SUM(sold_price) AS total_sales
+-- 	FROM staff S
+-- 	LEFT JOIN sold_cars ON S.id = seller
+-- WHERE role = 'Salesperson'
+-- GROUP BY name, role
+-- ORDER BY total_sales DESC;
+
+
+
+/*
+	Select the city, state and
+		count the total number of cars in each dealership
+		alias the count as car_count
+	
+	Use cars as the left table, and dealerships as the right table
+		choosing a join which will show every dealership
+		
+	Include a condition to count unsold cars
+	
+	Group by dealership city and state
+	Order by the car_count
+*/
+-- SELECT city, state, COUNT(C.id) AS car_count
+-- 	FROM cars C
+-- 	RIGHT JOIN dealerships D ON dealership_id = D.id
+-- WHERE sold IS NOT TRUE
+-- GROUP BY city, state
+-- ORDER BY car_count;
+
+
+
+
+
+
+
+/*
+	List:
+		- the brand and model of cars
+		- include the name of the seller,
+		- the city they work in
+		- the date of the sale
+	
+	Format the sold_date as DD-MM-YYYY using TO_CHAR()
+	
+	Use sold_cars as the left table and join other tables
+		show sold_cars when we have no record of the seller
+*/
+-- SELECT
+-- 	C.brand,
+-- 	C.model,
+-- 	-- S.name AS seller_name,
+-- 	-- D.city,
+-- 	TO_CHAR(SC.sold_date, 'DD-MM-YYYY') AS date_of_sale
+-- FROM sold_cars SC
+-- 	INNER JOIN cars C ON SC.cars_id = C.id;
+
+
+-- SELECT
+-- 	C.brand,
+-- 	C.model,
+-- 	S.name AS seller_name,
+-- 	-- D.city,
+-- 	TO_CHAR(SC.sold_date, 'DD-MM-YYYY') AS date_of_sale
+-- FROM sold_cars SC
+-- 	INNER JOIN cars C ON SC.cars_id = C.id
+-- 	LEFT JOIN staff S ON SC.seller = S.id;
+-- SELECT
+-- 	C.brand,
+-- 	C.model,
+-- 	S.name AS seller_name,
+-- 	D.city,
+-- 	TO_CHAR(SC.sold_date, 'DD-MM-YYYY') AS date_of_sale
+-- FROM sold_cars SC
+-- 	INNER JOIN cars C ON SC.cars_id = C.id
+-- 	LEFT JOIN staff S ON SC.seller = S.id
+-- 	LEFT JOIN dealerships D ON S.dealership_id = D.id;
+
+
+
+
+/*
+	Select the name, role and city from sold_cars
+	
+	Join with the staff and dealerships tables
+		use appropriate joins to show staff who have no dealership_id
+		
+	Include a where clause to find
+		- null values in sold_cars
+		- staff who have the role 'Salesperson'
+*/
+-- SELECT
+-- 	S.name,
+-- 	S.role,
+-- 	D.city
+-- FROM sold_cars SC
+-- 	FULL JOIN staff S ON SC.seller = S.id
+-- 	LEFT JOIN dealerships D ON S.dealership_id = D.id
+-- WHERE SC.id IS NULL
+-- 	AND S.role = 'Salesperson';
+
+
+
+
+/*
+	Show the city and state of dealerships
+		with a count of the cars sold
+		aliased as cars_sold
+		
+	Select from sold_cars
+		join with the relevant tables
+		
+	Include dealerships which have no sold cars
+	
+	Order the count in descending order
+		
+	Hint: you may need to join using a table not included in our columns
+*/
+-- SELECT
+-- 	D.city,
+-- 	D.state,
+-- 	COUNT(SC.id) AS cars_sold
+-- FROM sold_cars SC
+-- 	LEFT JOIN cars C ON SC.cars_id = C.id
+-- 	RIGHT JOIN dealerships D ON C.dealership_id = D.id
+-- GROUP BY D.city, D.state
+-- ORDER BY cars_sold DESC;
